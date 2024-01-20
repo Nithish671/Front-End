a3f566f8bb97788c04bab40bc2b99f3880b28a97
Nithish671 <113500526+Nithish671@users.noreply.github.com>
2024-01-20 10:25:14 +0530

    Add files via upload

---
+++ Javascript/JS_6_Code_Challange/js/main.js
@@ -0,0 +1,28 @@
+
+console.log("Satoru".charAt(Math.floor(Math.random() * 6)));
+console.log("Satoru".charAt(Math.floor(Math.random() * 6)));
+console.log("Satoru".charAt(Math.floor(Math.random() * 6)));
+console.log("Satoru".charAt(Math.floor(Math.random() * 6)));
+console.log("Satoru".charAt(Math.floor(Math.random() * 6)));
+console.log("Satoru".charAt(Math.floor(Math.random() * 6)));
+console.log("Satoru".charAt(Math.floor(Math.random() * 6)));
+console.log("Satoru".charAt(Math.floor(Math.random() * 6)));
+console.log("Satoru".charAt(Math.floor(Math.random() * 6)));
+
+const anyName = "Itachi Uchiha";
+
+const noOfC = anyName.length;
+
+console.log("\n");
+
+console.log(anyName.charAt(Math.random() * noOfC));
+console.log(anyName.charAt(Math.random() * noOfC));
+console.log(anyName.charAt(Math.random() * noOfC));
+console.log(anyName.charAt(Math.random() * noOfC));
+console.log(anyName.charAt(Math.random() * noOfC));
+console.log(anyName.charAt(Math.random() * noOfC));
+console.log(anyName.charAt(Math.random() * noOfC));
+console.log(anyName.charAt(Math.random() * noOfC));
+console.log(anyName.charAt(Math.random() * noOfC));
+console.log(anyName.charAt(Math.random() * noOfC));
+console.log(anyName.charAt(Math.random() * noOfC));
